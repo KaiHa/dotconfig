c88a7a7ffe1769956811648430b6ce9b67f766e5
Kai Harries <kai.harries@gmail.com>
2015-11-04 10:45:46 +0100

    emacs: move ensure-package-installed into myfunctions.el

--- emacs/myfunctions.el
+++ emacs/myfunctions.el
@@ -4,3 +4,16 @@
   (interactive "DDirectory: ")
   (shell-command
    (format "ctags -e -R %s" (directory-file-name dir-name))))
+
+
+;; Assure every given package in packages is installed
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.\n\nReturn a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
