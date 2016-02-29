3be048b85967b3cf65f90b58f9798c39828a7a6d
Kai Harries <kai.harries@gmail.com>
2016-02-29 22:09:55 +0100

    Add xmobar and screenlock

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,21 @@
 module Main (main) where
 
 import XMonad
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, ppOutput, ppTitle, shorten, xmobarColor, xmobarPP)
+import XMonad.Util.EZConfig    (additionalKeys)
+import XMonad.Util.Run         (hPutStrLn, spawnPipe)
 
 main :: IO ()
-main = xmonad defaults
+main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobarrc"
+  xmonad $ defaults
+    { logHook = dynamicLogWithPP xmobarPP
+               {ppOutput = hPutStrLn xmproc
+               ,ppTitle = xmobarColor "green" "" . shorten 50
+               }
+    } `additionalKeys`
+    [ ((controlMask .|. mod1Mask, xK_l), spawn "xscreensaver-command -lock")
+    ]
 
 defaults = defaultConfig
   {borderWidth        = 2
