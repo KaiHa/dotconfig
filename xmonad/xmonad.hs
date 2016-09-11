23b6bd18174ebf5a2ebcb9421d8440f9f22691a7
Kai Harries <kai.harries@gmail.com>
2016-09-11 21:09:31 +0200

    xmonad: Add web and emacs workspace

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,25 +2,20 @@
 
 module Main (main) where
 
-import Data.String.Utils            ( replace)
-import XMonad
-import XMonad.Actions.CycleWS       ( toggleWS)
-import XMonad.Actions.WindowBringer ( gotoMenu)
-import XMonad.Actions.WindowGo
-import XMonad.Hooks.DynamicLog      ( dynamicLog
-                                    , shorten
-                                    , statusBar
-                                    , wrap
-                                    , xmobarColor
-                                    , PP(..)
-                                    )
-import XMonad.Hooks.ManageDocks     ( docksEventHook )
-import XMonad.Hooks.FadeInactive    ( fadeInactiveLogHook)
-import XMonad.Hooks.ManageDocks     ( AvoidStruts)
-import XMonad.Layout.LayoutModifier ( ModifiedLayout)
-import XMonad.Layout.Magnifier      ( magnifiercz)
-import XMonad.Util.EZConfig         ( additionalKeys)
-import XMonad.Util.WorkspaceCompare ( getSortByXineramaPhysicalRule)
+import           Data.String.Utils (replace)
+import           XMonad
+import           XMonad.Actions.CycleWS (toggleWS)
+import           XMonad.Actions.WindowBringer (gotoMenu)
+import           XMonad.Actions.WindowGo (runOrRaise, raiseMaybe)
+import qualified XMonad.Hooks.DynamicLog as L
+import           XMonad.Hooks.FadeInactive (fadeInactiveLogHook)
+import           XMonad.Hooks.ManageDocks (AvoidStruts)
+import           XMonad.Hooks.ManageDocks (docksEventHook)
+import           XMonad.Layout.LayoutModifier (ModifiedLayout)
+import           XMonad.Layout.Magnifier (magnifiercz)
+import qualified XMonad.StackSet as W
+import           XMonad.Util.EZConfig (additionalKeys)
+import           XMonad.Util.WorkspaceCompare (getSortByXineramaPhysicalRule)
 
 
 main :: IO ()
@@ -29,43 +24,51 @@ main = xmonad =<< myxmobar defaults
 
 defaults = def
   { borderWidth        = 2
-  , focusFollowsMouse  = False
+  , focusFollowsMouse  = True
   , handleEventHook    = mconcat [docksEventHook, handleEventHook def]
   , layoutHook         = Tall 1 (3/100) (1/2) ||| Full
                          ||| Mirror (Tall 1 (3/100) (1/2))
                          ||| magnifiercz 1.5 (Tall 1 (3/100) (1/2))
-  , manageHook         = do
-                         (className =? "Firefox") --> doShift "1"
+  , manageHook         = composeAll
+                           [ (className =? "Firefox") --> doShift "web"
+                           , (className =? "Emacs")   --> doShift "emacs"
+                           ]
   , modMask            = mod4Mask
+  , startupHook        = windows $ W.view "web"
   , terminal           = "urxvtc"
+  , workspaces         = ["1", "2", "3", "4", "5", "6", "7", "8", "emacs", "web"]
   } `additionalKeys`
-  [ ((controlMask .|. mod1Mask, xK_l), spawn "xautolock -locknow")
-  , ((mod4Mask, xK_o),                 gotoMenu)
-  , ((mod4Mask, xK_Escape),            toggleWS)
-  , ((noModMask, xK_LowerVol),         spawn "amixer -D pulse sset Master 2%-")
-  , ((noModMask, xK_RaiseVol),         spawn "amixer -D pulse sset Master 2%+")
-  , ((noModMask, xK_Launch2),          runOrRaise "firefox" (className =? "Firefox"))
+  [ ((mod4Mask,                 xK_0),       windows $ W.greedyView "web")
+  , ((mod4Mask .|. shiftMask,   xK_0),       windows $ W.shift "web")
+  , ((controlMask .|. mod1Mask, xK_l),       spawn "xautolock -locknow")
+  , ((mod4Mask,                 xK_o),       gotoMenu)
+  , ((mod4Mask,                 xK_Escape),  toggleWS)
+  , ((mod4Mask,                 xK_F8),      spawn "amixer sset Master 2%-")
+  , ((mod4Mask,                 xK_F9),      spawn "amixer sset Master 2%+")
+  , ((mod4Mask .|. mod1Mask,    xK_Return),  spawn "urxvtc -bg black -fg white")
+  , ((noModMask,                xK_Launch2), runOrRaise "firefox" (className =? "Firefox"))
+  , ((noModMask,                xK_Launch3), raiseMaybe (spawn "emacsclient -c") (className =? "Emacs"))
   ]
 
 
 myxmobar :: LayoutClass l Window
        => XConfig l -> IO (XConfig (ModifiedLayout AvoidStruts l))
 myxmobar conf =
-  statusBar "xmobar ~/.config/xmonad/xmobarrc" myPP toggleStrutsKey conf
+  L.statusBar "xmobar ~/.config/xmonad/xmobarrc" myPP toggleStrutsKey conf
   where
     toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b )
 
 
-myPP:: PP
-myPP = def { ppCurrent = xmobarColor "yellow" "#666600" . wrap "[" "]"
-           , ppLayout  = replace "NoMaster " ""
-           , ppSep     = " | "
-           , ppWsSep   = ""
-           , ppSort    = getSortByXineramaPhysicalRule
-           , ppTitle   = xmobarColor "white"  "" . shorten 60
-           , ppVisible = xmobarColor "#cccccc" "#666600". wrap "." "."
-           , ppHidden  = wrap " " " "
-           , ppUrgent  = xmobarColor "red" "yellow"
+myPP:: L.PP
+myPP = def { L.ppCurrent = L.xmobarColor "yellow" "#666600" . L.wrap "[" "]"
+           , L.ppLayout  = replace "NoMaster " ""
+           , L.ppSep     = " | "
+           , L.ppWsSep   = ""
+           , L.ppSort    = getSortByXineramaPhysicalRule
+           , L.ppTitle   = L.xmobarColor "white"  "" . L.shorten 60
+           , L.ppVisible = L.xmobarColor "#cccccc" "#666600". L.wrap "." "."
+           , L.ppHidden  = L.wrap " " " "
+           , L.ppUrgent  = L.xmobarColor "red" "yellow"
            }
 
 
@@ -74,3 +77,4 @@ xK_LowerVol    = stringToKeysym "XF86AudioLowerVolume"
 xK_RaiseVol    = stringToKeysym "XF86AudioRaiseVolume"
 xK_ScreenSaver = stringToKeysym "XF86ScreenSaver"
 xK_Launch2     = stringToKeysym "XF86Launch2"
+xK_Launch3     = stringToKeysym "XF86Launch3"
