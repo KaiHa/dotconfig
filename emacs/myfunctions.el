b3295d295edc67b5098e68ab3826a65912ae3cc9
Kai Harries <kai.harries@gmail.com>
2017-11-19 16:48:25 +0100

    emacs/notmuch: remove the overridden (CRLF workaround) functions

--- emacs/myfunctions.el
+++ emacs/myfunctions.el
@@ -41,52 +41,6 @@
     (visual-line-mode -1)))
 
 
-;; My version of mime-to-mml which fixes the line endings
-(defun mime-to-mml (&optional handles)
-  "Translate the current buffer (which should be a message) into MML.
-If HANDLES is non-nil, use it instead reparsing the buffer."
-  ;; First decode the head.
-  (save-excursion (icalendar--clean-up-line-endings))
-  (save-restriction
-    (message-narrow-to-head)
-    (let ((rfc2047-quote-decoded-words-containing-tspecials t))
-      (mail-decode-encoded-word-region (point-min) (point-max))))
-  (unless handles
-    (setq handles (mm-dissect-buffer t)))
-  (goto-char (point-min))
-  (search-forward "\n\n" nil t)
-  (delete-region (point) (point-max))
-  (if (stringp (car handles))
-      (mml-insert-mime handles)
-    (mml-insert-mime handles t))
-  (mm-destroy-parts handles)
-  (save-restriction
-    (message-narrow-to-head)
-    ;; Remove them, they are confusing.
-    (message-remove-header "Content-Type")
-    (message-remove-header "MIME-Version")
-    (message-remove-header "Content-Disposition")
-    (message-remove-header "Content-Transfer-Encoding")))
-
-
-;; My version of notmuch-show-view-raw-message which fixes the line endings
-(defun notmuch-show-view-raw-message ()
-  "View the original source of the current message."
-  (interactive)
-  (let* ((id (notmuch-show-get-message-id))
-	 (buf (get-buffer-create (concat "*notmuch-raw-" id "*")))
-	 (inhibit-read-only t))
-    (switch-to-buffer buf)
-    (erase-buffer)
-    (let ((coding-system-for-read 'no-conversion))
-      (call-process notmuch-command nil t nil "show" "--format=raw" id))
-    (save-excursion (icalendar--clean-up-line-endings))
-    (goto-char (point-min))
-    (set-buffer-modified-p nil)
-    (setq buffer-read-only t)
-    (view-buffer buf 'kill-buffer-if-not-modified)))
-
-
 (defun addresssearch (name)
   "Search for email addresses."
   (interactive "sSearch for name: ")
